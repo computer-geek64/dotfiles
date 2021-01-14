8aaae47a9ce23c21b8896a43389ce995c3c26516
Ashish D'Souza <sudouser512@gmail.com>
2021-01-13 20:47:46 -0500

    Added conky

---
+++ home/ashish/.conky/temp_conky_draw_config.lua
@@ -0,0 +1,48 @@
+elements = {
+    {
+        kind = 'ring_graph',
+        conky_value = 'cpu',
+        center = {x = 170, y = 140},
+        radius = 80,
+
+        bar_color = 0x00E5FF,
+        bar_alpha = 1,
+        bar_thickness = 15,
+
+        background_color = 0xFFFFFF,
+        background_alpha = 1,
+        background_thickness = 70,
+
+        critical_threshold = 80,
+        change_color_on_critical = true,
+        change_alpha_on_critical = false,
+        change_thickness_on_critical = false,
+
+        bar_color_critical = 0xFF0000,
+
+        background_color_critical = 0xFFFFFF,
+    },
+    --[[{
+        kind = 'ring_graph',
+        conky_value = 'cpu',
+        center = {x = 120, y = 370},
+        radius = 80,
+
+        bar_color = 0x00E5FF,
+        bar_alpha = 1,
+        bar_thickness = 15,
+
+        background_color = 0xFFFFFF,
+        background_alpha = 1,
+        background_thickness = 70,
+
+        critical_threshold = 80,
+        change_color_on_critical = true,
+        change_alpha_on_critical = false,
+        change_thickness_on_critical = false,
+
+        bar_color_critical = 0xFF0000,
+
+        background_color_critical = 0xFFFFFF,
+    },]]
+}
